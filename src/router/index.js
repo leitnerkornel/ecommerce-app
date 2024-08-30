65b8404e3556e6ed1da945d253d6da976b1d2475
Kornel Leitner <leitner.kornel@gmail.com>
2024-08-30 21:34:07 +0200

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'root',
+      redirect: '/products',
+      component: () => import('../views/ProductsView.vue')
+    },
+    {
+      path: '/products',
+      name: 'products',
+      component: () => import('../views/ProductsView.vue')
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: () => import('../views/CartView.vue')
+    }
+  ]
+})
+
+export default router
